7456a25f8801e6c0a87540db83af72dbbb3ed14f
vne-cloud <pikachu1919@mail.ru>
2020-11-24 18:54:37 +0300

    4.1

--- core/EquationInterface.php
+++
@@ -1,7 +0,0 @@
-<?php namespace core;
-	
-interface EquationInterface {
-    public function solve($a, $b, $c);
-}
-
-?>
--- core/LogInterface.php
+++
@@ -1,8 +0,0 @@
-<?php namespace core;
-	
-interface LogInterface {
-   public static function log(string $str);
-   public static function write();
-}
-
-?>
